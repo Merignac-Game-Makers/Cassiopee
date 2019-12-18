5d2c8ea8e459be61c31471f26dd4152f264ad52e
Robeen <robert.gras.33@gmail.com>
2019-12-18 22:48:06 +0100

    Préparation quete 1

---
+++ Assets/Scripts/Quests/QuestBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class QuestBase : MonoBehaviour
+{
+	public enum QuestStatus { None, Accepted, Refused, Done }
+	[HideInInspector]
+	public QuestStatus questStatus = QuestStatus.None;
+	public void RefuseQuest() {
+		questStatus = QuestStatus.Refused;
+	}
+	public void AcceptQuest() {
+		questStatus = QuestStatus.Accepted;
+	}
+	public void QuestDone() {
+		questStatus = QuestStatus.Done;
+	}
+
+}
