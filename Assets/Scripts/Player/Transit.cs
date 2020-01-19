d8d55b1aab6816e871d9d9c73d7cc0882319fb19
Thundzz <yacinenew@gmail.com>
2020-01-19 14:26:34 +0100

    Le SAS n'est plus utilisé dans le code.

--- Assets/Scripts/Player/Transit.cs
+++ Assets/Scripts/Player/Transit.cs
@@ -5,11 +5,8 @@ using Cinemachine;
 
 public class Transit : MonoBehaviour
 {
-
-	public Collider sas;
 	public Transform destinationPoint;
 	public CinemachineVirtualCamera localCam;
-
 	private PlayerManager player;
 	private CinemachineBrain cinemachineBrain;      // cinemachine
 	private Stack<CinemachineVirtualCamera> vCams => CameraController.Instance.vCams;
@@ -28,7 +25,7 @@ public class Transit : MonoBehaviour
 
 	public void OnTriggerEnter(Collider other) {
 		if (other.gameObject.GetComponentInChildren<PlayerManager>()) {
-			if (sas.bounds.Contains(other.transform.position)) {
+			if (IsFacingForward(other)) {
 				if (localCam) {														// s'il existe une caméra dédiée pour ce transit
 					localCam.gameObject.SetActive(true);                            // activer la caméra locale
 					vCams.Peek().gameObject.SetActive(false);                       // désactiver la caméra précédente
@@ -40,4 +37,10 @@ public class Transit : MonoBehaviour
 			}
 		}
 	}
+
+	public bool IsFacingForward(Collider other){
+		var collisionDirection = transform.position - other.transform.position;
+		return Vector3.Dot(collisionDirection, transform.forward) > 0;
+	}
+
 }
