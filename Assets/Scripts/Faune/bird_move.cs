a6d03e3366869361b940d41a560c0848381165bc
AKTasos <tasos84@hotmail.com>
2020-01-29 22:19:03 +0100

    Ajout aigle plus script

---
+++ Assets/Scripts/Faune/bird_move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bird_move : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 axis;
+    public float moveSpeed = 5f;
+    public float turnSpeed = 5f;
+
+    private readonly Vector3 down = new Vector3(0,-1,0);
+    void Update()
+    {
+        
+        Vector3 direction = target.position - transform.position;
+        float dist = Vector3.Distance(target.position, transform.position);
+        
+        if (dist >= 10f)
+        {
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = rotation;
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+
+        else
+        {
+            
+            transform.transform.RotateAround(target.position, axis, turnSpeed);
+            transform.transform.Rotate(0, 2, 0, Space.Self);
+            //transform.rotation = Quaternion.Lerp(Vector3.forward, transform.up, Time.time * turnSpeed);
+            transform.transform.forward = Vector3.Cross(down, direction);
+        }
+        //transform.Translate(Vector3.up * turnSpeed * Time.deltaTime);
+    }
+
+
+
+
+
+
+
+
+
+
+}    
