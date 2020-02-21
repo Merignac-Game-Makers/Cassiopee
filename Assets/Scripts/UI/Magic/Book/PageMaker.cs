7030942ce8b5bf28313d60c92d96fba6338b7069
Robeen <robert.gras.33@gmail.com>
2020-02-21 09:41:01 +0100

    Maison de notable

--- Assets/Scripts/UI/Magic/Book/PageMaker.cs
+++ Assets/Scripts/UI/Magic/Book/PageMaker.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using static PageMaker.Side;
 
 public abstract class PageMaker : MonoBehaviour
 {
 	public enum Side { left, right }
-
 	public GameObject leftPage;
 	public VerticalLayoutGroup leftContent;
-
 	public GameObject rightPage;
 	public VerticalLayoutGroup rightContent;
-
 
 	public abstract void Make();
 
@@ -28,6 +21,7 @@ public abstract class PageMaker : MonoBehaviour
 		Clear(leftContent);
 		Clear(rightContent);
 	}
+
 	public virtual void Clear(VerticalLayoutGroup content) {
 		foreach (Transform child in content.transform) {
 			Destroy(child.gameObject);
