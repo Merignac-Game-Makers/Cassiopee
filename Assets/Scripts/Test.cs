0b7a23ab5b53a67bbca36af5dc5e32c32de0c2ad
Robeen <robert.gras.33@gmail.com>
2020-02-26 18:18:22 +0100

    Maison, ...

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Test : MonoBehaviour
+{
+	public Transform inPoint;
+	PlayerManager player;
+
+	private void OnMouseDown() {
+		Debug.Log("test");
+		player = PlayerManager.Instance;
+		player.m_Agent.SetDestination(inPoint.position);
+		player.inTransit = true;
+	}
+	//public void OnMouseDown() {
+	//	Debug.Log("test");
+	//}
+}
